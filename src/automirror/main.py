18e8eabb35b22dac95fd2c670f26480bb5f8ed75
syhanjin <2819469337@qq.com>
2024-10-17 01:29:24 +0800

    支持解析配置文件，配置文件路径可以通过程序参数设置（具有默认值）

--- src/automirror/main.py
+++ src/automirror/main.py
@@ -1,7 +1,43 @@
 # -*- coding: utf-8 -*-
+import argparse
+import tomllib
+from pathlib import Path
+
+import httpx
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=Path, default=Path('./config.toml'), help='同步用配置文件路径')
+
+config = Path('./config.toml')
+target_base_url = ''
+origin_base_url = ''
+target_client = httpx.AsyncClient(timeout=None)
+
 
 async def main(argv):
-    print(argv)
+    global config, target_base_url, origin_base_url, target_client
+    # 解析参数
+    args = parser.parse_args(argv)
+    config: Path = args.config
+    # 保证config存在
+    if not config.exists():
+        raise FileNotFoundError("没有找到配置文件")
+    with config.open('rb') as f:
+        data = tomllib.load(f)
+    assert data.get('config'), '配置文件中没有 config'
+    target_base_url = data['config'].get('target_base_url')
+    assert target_base_url, 'target_base_url未配置'
+    origin_base_url = data['config'].get('origin_base_url')
+    assert origin_base_url, 'origin_base_url未配置'
+    token = data['config'].get('token')
+    assert token, '认证token未配置'
+    target_client = httpx.AsyncClient(timeout=None, headers={
+        'Authorization': f'token {token}'
+    })
+    resp = await target_client.get(f"{target_base_url}/user")
+    assert resp.status_code == 200, f'认证不成功, {resp.status_code=}'
+
+    assert data.get('mirrors'), '没有配置镜像列表'
 
 
 if __name__ == '__main__':
