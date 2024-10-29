14e752655d5dcc3bfd84c72856f5958b6d3f3742
syhanjin <2819469337@qq.com>
2024-10-29 23:21:34 +0800

    使用单独的configs模块读取和校验配置文件

---
+++ src/automirror/configs.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import dataclasses
+import logging
+import token
+from enum import StrEnum
+from pathlib import Path
+from urllib.parse import urlparse
+from httpx import AsyncClient
+
+
+class MirrorType(StrEnum):
+    ORG = 'org'
+    REPO = 'repo'
+
+
+@dataclasses.dataclass
+class Mirror:
+    type: MirrorType
+    origin: str
+    target: str | None = None
+    url: str | None = None
+
+    def __repr__(self):
+        if self.origin:
+            return f'{self.type if isinstance(type, MirrorType) else ""}源({self.origin} -> {self.target})'
+        else:
+            return f'源({self.type=} {self.origin=} {self.target=} {self.url=})'
+
+    def validate(self) -> bool:
+        if not self.origin:
+            logging.warning(f'{self}的名字为空，将跳过同步')
+            return False
+        if not self.target:
+            self.target = self.origin
+        if self.type not in MirrorType:
+            logging.warning(f'{self}的类型不在可选范围，将跳过同步')
+            return False
+        self.type = MirrorType(self.type)
+        if self.type == MirrorType.REPO:
+            if not self.url:
+                logging.warning(f'{self}的clone_url为空，将跳过同步')
+                return False
+            parsed = urlparse(self.url)
+            if not bool(parsed.scheme and parsed.netloc):
+                logging.warning(f'{self}的clone_url不合法，将跳过同步')
+                return False
+        return True
+
+
+class Config:
+    target_base_url: str
+    origin_base_url: str
+    token: str
+
+    mirrors: list[Mirror]
+
+    target_client: AsyncClient
+    origin_client: AsyncClient = AsyncClient(timeout=None)
+
+    @classmethod
+    def load(cls, config_path: Path):
+        if not config_path.is_file():
+            raise FileNotFoundError(f"没有找到配置文件 {config_path}")
+        with config_path.open('rb') as f:
+            import tomllib
+            raw_config = tomllib.load(f)
+        assert raw_config.get('config'), '配置文件中没有 config 项'
+        cls.target_base_url = raw_config['config'].get('target_base_url')
+        assert cls.target_base_url, 'target_base_url未配置'
+        cls.origin_base_url = raw_config['config'].get('origin_base_url')
+        assert cls.origin_base_url, 'origin_base_url未配置'
+        cls.token = raw_config['config'].get('token')
+        assert cls.token, '认证token未配置'
+        assert raw_config.get('mirrors'), '没有配置镜像列表'
+        cls.mirrors = []
+        for mirror in raw_config['mirrors']:
+            mirror_obj = Mirror(
+                type=mirror.get('type'),
+                origin=mirror.get('origin'),
+                target=mirror.get('target'),
+                url=mirror.get('url')
+            )
+            if mirror_obj.validate():
+                cls.mirrors.append(mirror_obj)
+
+    @classmethod
+    async def check_token(cls) -> bool:
+        # f'认证不成功, {resp.status_code=}'
+        if not token:
+            return False
+        cls.target_client = AsyncClient(timeout=None, headers={
+            'Authorization': f'token {cls.token}'
+        })
+        resp = await cls.target_client.get(f"{cls.target_base_url}/user")
+        if resp.status_code != 200:
+            return False
+        return True
--- src/automirror/main.py
+++ src/automirror/main.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import argparse
 import logging
-import tomllib
 import asyncio
 from pathlib import Path
-
-import httpx
 
 __doc__ = """
 AutoMirror v0.1.0
@@ -30,26 +27,22 @@ target = "your_target_org_name" # 同步到镜像站后属于的Org
 --- --- ---
 """.strip()
 
+from automirror.configs import Config, MirrorType
+
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description=__doc__)
 parser.add_argument('-c', '--config', type=Path, default=Path('./config.toml'), help='同步用配置文件路径')
-
-config: Path = Path('./config.toml')
-target_base_url = ''
-origin_base_url = ''
-target_client = httpx.AsyncClient(timeout=None)
-origin_client = httpx.AsyncClient(timeout=None)
 
 
 # TODO: 统一logging风格
 
 async def check_target(target) -> list[dict[str, str]]:
     # 检查target是否存在
-    resp = await target_client.get(f'{target_base_url}/orgs/{target}')
+    resp = await Config.target_client.get(f'{Config.target_base_url}/orgs/{target}')
     assert resp.status_code in [200, 404], f'出了点小问题？{target=}, {resp.status_code=}'
     target_repos = []
     if resp.status_code == 404:
         # 创建org
-        resp = await target_client.post(f'{target_base_url}/orgs/', json={'username': target})
+        resp = await Config.target_client.post(f'{Config.target_base_url}/orgs/', json={'username': target})
         assert resp.status_code == 201, f'创建org失败, {resp.status_code=}'
     else:
         target_repos = await get_target_org_repos(target)
@@ -58,9 +51,9 @@ async def check_target(target) -> list[dict[str, str]]:
 
 async def get_target_org_repos(target):
     repos = []
-    url = f'{target_base_url}/orgs/{target}/repos'
+    url = f'{Config.target_base_url}/orgs/{target}/repos'
     while url:
-        resp = await target_client.get(url)
+        resp = await Config.target_client.get(url)
         assert resp.status_code == 200, f'获取target_org失败, {resp.status_code=}'
         repos.extend(resp.json())
         if 'next' in resp.links:
@@ -71,9 +64,9 @@ async def get_target_org_repos(target):
 
 
 async def get_origin_org_repos_iter(origin):
-    url = f'{origin_base_url}/orgs/{origin}/repos'
+    url = f'{Config.origin_base_url}/orgs/{origin}/repos'
     while url:
-        resp = await origin_client.get(url)
+        resp = await Config.origin_client.get(url)
         assert resp.status_code == 200, f'获取源org仓库失败, {resp.status_code=}'
         for repo in resp.json():
             yield repo
@@ -84,8 +77,8 @@ async def get_origin_org_repos_iter(origin):
 
 
 async def repo_migrate(clone_addr, repo_name, repo_owner):
-    resp = await target_client.post(
-        f'{target_base_url}/repos/migrate/',
+    resp = await Config.target_client.post(
+        f'{Config.target_base_url}/repos/migrate/',
         json={
             'clone_addr': clone_addr,
             'mirror': True,
@@ -100,105 +93,70 @@ async def repo_migrate(clone_addr, repo_name, repo_owner):
 
 
 async def repo_delete(repo_name, repo_owner):
-    resp = await target_client.delete(f'{target_base_url}/repos/{repo_owner}/{repo_name}/')
+    resp = await Config.target_client.delete(f'{Config.target_base_url}/repos/{repo_owner}/{repo_name}/')
     if resp.status_code != 204:
         logging.error(f'DeleteFailed - {repo_owner}/{repo_name} - {resp.status_code=}')
         return
     logging.info(f"Deleted - {repo_owner}/{repo_name}")
 
 
-async def update_org(origin, target):
+async def update_org(mirror):
     get_origin_org_repos_exception = None
     try:
-        target_repos = await check_target(target)
+        target_repos = await check_target(mirror.target)
         target_repo_names = [x['name'] for x in target_repos]
     except Exception as e:
-        logging.error(f'同步失败({origin} -> {target})：检查target时发生错误 {e}')
+        logging.error(f'同步{mirror}失败：检查target时发生错误 {e}')
         return
-    async with asyncio.TaskGroup() as tg:
-        try:
-            async for repo in get_origin_org_repos_iter(origin):
-                if repo['name'] in target_repo_names:
-                    target_repo_names.remove(repo['name'])
-                    logging.info(f"Existed - {target}/{repo['name']}")
-                else:
-                    tg.create_task(repo_migrate(repo['clone_url'], repo['name'], target))
-        except Exception as e:
-            get_origin_org_repos_exception = e
-        # 删除不存在的repo
-        for repo_name in target_repo_names:
-            tg.create_task(repo_delete(repo_name, target))
-
-    if get_origin_org_repos_exception:
-        logging.error(f'同步不完全({origin} -> {target})：获取origin_repos时发生错误 {get_origin_org_repos_exception}')
-    else:
-        logging.info(f'同步成功({origin} -> {target})！')
-
-
-async def update_repo(origin, origin_url, target):
-    from urllib.parse import urlparse
-    parsed = urlparse(origin_url)
-    if not bool(parsed.scheme and parsed.netloc):
-        logging.error(f'同步失败({origin} -> {target})：{origin_url=} 不是合法的url')
-        return
+    tg = asyncio.TaskGroup()
     try:
-        target_repos = await check_target(target)
+        async for repo in get_origin_org_repos_iter(mirror.origin):
+            if repo['name'] in target_repo_names:
+                target_repo_names.remove(repo['name'])
+                logging.info(f"Existed - {mirror.target}/{repo['name']}")
+            else:
+                tg.create_task(repo_migrate(repo['clone_url'], repo['name'], mirror.target))
+    except Exception as e:
+        get_origin_org_repos_exception = e
+    # 删除不存在的repo
+    for repo_name in target_repo_names:
+        tg.create_task(repo_delete(repo_name, mirror.target))
+    
+    if get_origin_org_repos_exception:
+        logging.error(f'同步{mirror}不完全：获取origin_repos时发生错误 {get_origin_org_repos_exception}')
+    else:
+        logging.info(f'同步{mirror}成功！')
+
+
+async def update_repo(mirror):
+    try:
+        target_repos = await check_target(mirror.target)
         target_repo_names = [x['name'] for x in target_repos]
     except Exception as e:
-        logging.error(f'同步失败({origin} -> {target})：检查target时发生错误 {e}')
+        logging.error(f'同步{mirror}失败：检查target时发生错误 {e}')
         return
     # TODO: 避免获取全部target_repo再进行检查
-    if origin in target_repo_names:
-        logging.info(f"Existed - {target}/{origin}")
+    if mirror.origin in target_repo_names:
+        logging.info(f"Existed - {mirror.target}/{mirror.origin}")
     else:
-        await repo_migrate(origin_url, origin, target)
+        await repo_migrate(mirror.url, mirror.origin, mirror.target)
+    logging.info(f'同步{mirror}成功！')
 
 
 async def main(argv):
     # 关闭httpx的输出
     logging.getLogger("httpx").setLevel(logging.CRITICAL + 1)
-    global config, target_base_url, origin_base_url, target_client
     # 解析参数
     args = parser.parse_args(argv)
-    config = args.config
-    # 保证config存在
-    if not config.exists():
-        raise FileNotFoundError("没有找到配置文件")
-    with config.open('rb') as f:
-        data = tomllib.load(f)
-    assert data.get('config'), '配置文件中没有 config'
-    target_base_url = data['config'].get('target_base_url')
-    assert target_base_url, 'target_base_url未配置'
-    origin_base_url = data['config'].get('origin_base_url')
-    assert origin_base_url, 'origin_base_url未配置'
-    token = data['config'].get('token')
-    assert token, '认证token未配置'
-    target_client = httpx.AsyncClient(timeout=None, headers={
-        'Authorization': f'token {token}'
-    })
-    resp = await target_client.get(f"{target_base_url}/user")
-    assert resp.status_code == 200, f'认证不成功, {resp.status_code=}'
-
-    assert data.get('mirrors'), '没有配置镜像列表'
-    # 以上是开始同步前可以检查出的配置问题，使用assert检查，检查不通过则直接终止
+    Config.load(args.config)
+    await Config.check_token()
     logging.info("开始同步")
-    for mirror in data['mirrors']:
-        if not mirror.get('origin'):
-            # 镜像源名字为空，直接跳过，不警告
-            return
-        if not mirror.get('target'):
-            mirror['target'] = mirror['origin']
-        if mirror.get('type') not in ['repo', 'org']:
-            logging.error(
-                f'同步失败({mirror["origin"]} -> {mirror["target"]})：类型必须为repo或者org，实际为：{mirror["type"]}')
-            return
-        if mirror['type'] == 'repo':
-            logging.info(f'同步Repo({mirror["origin"]} -> {mirror["target"]})...')
-            await update_repo(mirror['origin'], mirror.get('url'), mirror['target'])
-            # print(result)
-        elif mirror['type'] == 'org':
-            logging.info(f'同步Org({mirror["origin"]} -> {mirror["target"]})...')
-            await update_org(mirror['origin'], mirror['target'])
+    for mirror in Config.mirrors:
+        logging.info(f"开始同步{mirror}...")
+        if mirror.type == MirrorType.REPO:
+            await update_repo(mirror)
+        elif mirror.type == MirrorType.ORG:
+            await update_org(mirror)
 
 
 if __name__ == '__main__':
